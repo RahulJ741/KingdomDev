09806287925c2399cc69d569c73e40d798b3e418
RahulJ741 <rahul.j@infiny.in>
2017-05-23 11:41:52 +0530

    contact page and sports page

--- app/controllers/staticpage_controller.rb
+++ app/controllers/staticpage_controller.rb
@@ -1,22 +1,24 @@
 class StaticpageController < ApplicationController
 
   def index
-
   end
 
   def city
-
   end
 
   def privacy
-
   end
 
   def lookandbrand
-
   end
 
   def whoweare
-
   end
+
+  def contact
+  end
+
+  def sports
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get 'privacy' => 'staticpage#privacy'
   get 'lookandbrand' => 'staticpage#lookandbrand'
   get 'whoweare' => 'staticpage#whoweare'
+  get 'contact' => 'staticpage#contact'
+  get 'sports' => 'staticpage#sports'
 end
