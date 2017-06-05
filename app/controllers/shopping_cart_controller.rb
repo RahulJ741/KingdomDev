20f2c96944026d231a3ae2ef98f8fbf7da2edee6
RahulJ741 <rahul.j@infiny.in>
2017-06-05 19:07:22 +0530

    changes

--- app/controllers/shopping_cart_controller.rb
+++ app/controllers/shopping_cart_controller.rb
@@ -1,6 +1,13 @@
 class ShoppingCartController < ApplicationController
   def index
-    @cart = ShoppingCart.where(:user_id => session[:user_id])
+    # @cart = ShoppingCart.where(:user_id => session[:user_id])
+    if session[:user_id]
+      @current_user = User.find(session["user_id"])
+      puts session[:user_id]
+      @cart = ShoppingCart.where(:user_id => session[:user_id])
+    else
+      @current_user = nil
+    end
   end
 
   def add_cart
