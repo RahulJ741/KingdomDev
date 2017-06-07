60fa81b1ec6772578a97320447b588539c3e5ade
RahulJ741 <rahul.j@infiny.in>
2017-06-07 14:13:31 +0530

    ui fixes

--- app/models/shopping_cart.rb
+++ app/models/shopping_cart.rb
@@ -2,6 +2,11 @@ class ShoppingCart < ApplicationRecord
 
   belongs_to :room
   belongs_to :user
-  # has_and_belongs_to_many :rooms 
+  # has_and_belongs_to_many :rooms
   # has_many :rooms
+
+
+
+  # scope :from_date, ->
+  # scope :status, -> (status) { where status: status }
 end
