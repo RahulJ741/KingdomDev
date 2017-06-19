2982df69de312749a598f44577ce2d6df57cd9fd
RahulJ741 <rahul.j@infiny.in>
2017-06-19 16:08:52 +0530

    chang

--- db/migrate/20170619101243_droptables.rb
+++ db/migrate/20170619101243_droptables.rb
@@ -1,27 +1,27 @@
 class Droptables < ActiveRecord::Migration[5.0]
   def change
     # drop_table :athletics_supporters_package_price_list_all
-    drop_table :event_shopping_carts
-    drop_table :event_ticket_request
-    drop_table :events
-    drop_table :exclisive_second_old
-    drop_table :exclusive_old
-    drop_table :hotels_amenities
-    drop_table :kngd_hotels
-    drop_table :kngdm_hotels
-    drop_table :kngdsg_hotels
-    drop_table :login_attempts
-    drop_table :mytable
-    drop_table :newsletter
-    drop_table :own_package_price_list
-    drop_table :packages_opening_dual_price_list
-    drop_table :packages_rugby_dual_price_list
-    drop_table :packages_swimming_dual_price
-    drop_table :settings
-    drop_table :staticpages
-    drop_table :users_groups
-    drop_table :hotels_images_old
-    drop_table :groups
+    # drop_table :event_shopping_carts
+    # drop_table :event_ticket_request
+    # drop_table :events
+    # drop_table :exclisive_second_old
+    # drop_table :exclusive_old
+    # drop_table :hotels_amenities
+    # drop_table :kngd_hotels
+    # drop_table :kngdm_hotels
+    # drop_table :kngdsg_hotels
+    # drop_table :login_attempts
+    # drop_table :mytable
+    # drop_table :newsletter
+    # drop_table :own_package_price_list
+    # drop_table :packages_opening_dual_price_list
+    # drop_table :packages_rugby_dual_price_list
+    # drop_table :packages_swimming_dual_price
+    # drop_table :settings
+    # drop_table :staticpages
+    # drop_table :users_groups
+    # drop_table :hotels_images_old
+    # drop_table :groups
 
   end
 end
